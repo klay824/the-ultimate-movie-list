e167193d6cca97be97eaf6af751fc19011ee331e
katelyn chadwell <kchadwell0226@gmail.com>
2021-05-16 12:01:57 -0500

    got routes working

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,14 @@ const router = require('express').Router();
 
 // const routes here
 const homeRoutes = require('./homeRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
+const resultsRoutes = require('./resultsRoutes');
 
 
 // router.use here
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/results', resultsRoutes);
 
 
 router.use((req, res) => {
